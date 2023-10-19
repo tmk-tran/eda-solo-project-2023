f5b39500c5a64c2f267d248e30ba038d67061900
T Mark <tmk1.tran@gmail.com>
2023-10-19 16:08:14 -0500

    assigned links to navbar component, tested and they all work. games is empty for now, will need to add some additional links for game page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import PersonIcon from "@mui/icons-material/Person";
@@ -6,9 +7,9 @@ import RestoreIcon from "@mui/icons-material/Restore";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import GpsFixedIcon from "@mui/icons-material/GpsFixed";
 import Paper from "@mui/material/Paper";
-import InfoIcon from '@mui/icons-material/Info';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBullseye } from '@fortawesome/free-solid-svg-icons';
+import InfoIcon from "@mui/icons-material/Info";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBullseye } from "@fortawesome/free-solid-svg-icons";
 
 export default function SimpleBottomNavigation() {
   return (
@@ -18,16 +19,54 @@ export default function SimpleBottomNavigation() {
     >
       <BottomNavigation
         showLabels
+        style={{ backgroundColor: "#2c2b2b" }}
         //   value={value}
         //   onChange={(event, newValue) => {
         //     setValue(newValue);
         //   }}
-      >s
-        <BottomNavigationAction label="Profile" icon={<PersonIcon />} />
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Train" icon={<GpsFixedIcon style={{ fontSize: "34px" }}/>} />
-        <BottomNavigationAction label="Games" icon={<FontAwesomeIcon icon={faBullseye} style={{ fontSize: "22px" }} />} />
-        <BottomNavigationAction label="Info" icon={<InfoIcon />} />
+      >
+        <BottomNavigationAction
+          label="Profile"
+          icon={<PersonIcon />}
+          style={{ color: "ghostwhite" }}
+          component={Link}
+          to="/profile"
+        />
+        <BottomNavigationAction
+          label="History"
+          icon={<RestoreIcon />}
+          style={{ color: "ghostwhite" }}
+          component={Link}
+          to="/history"
+        />
+        <BottomNavigationAction
+          label="Train"
+          icon={<GpsFixedIcon style={{ fontSize: "34px" }} />}
+          style={{ color: "ghostwhite" }}
+          component={Link}
+          to="/3-ring"
+        />
+        <BottomNavigationAction
+          label="Games"
+          style={{ color: "ghostwhite" }}
+          icon={
+            <FontAwesomeIcon
+              icon={faBullseye}
+              style={{
+                fontSize: "21px",
+                color: "ghostwhite",
+                marginBottom: "2px",
+              }}
+            />
+          }
+        />
+        <BottomNavigationAction
+          label="Info"
+          icon={<InfoIcon />}
+          style={{ color: "white" }}
+          component={Link}
+          to="/about"
+        />
       </BottomNavigation>
     </Paper>
   );
