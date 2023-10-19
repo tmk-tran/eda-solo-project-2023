3d518eadd4de0cecb9e9c1dedcb6008c47bbb7e6
T Mark <tmk1.tran@gmail.com>
2023-10-19 10:59:06 -0500

    added table display to results component, for scores

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,12 +1,20 @@
-import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
+import EmojiEventsOutlinedIcon from "@mui/icons-material/EmojiEventsOutlined";
 import {
-    Card,
-    CardContent,
-    TextField,
-    FormControl,
-    Button,
-    Typography,
-  } from "@mui/material";
+  Card,
+  CardContent,
+  TextField,
+  FormControl,
+  Button,
+  Typography,
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TableCell,
+} from "@mui/material";
+
 import "./Results.css";
 
 export default function Results() {
@@ -15,7 +23,30 @@ export default function Results() {
       <h1 className="results-header">Results</h1>
       <Card>
         <CardContent>
-            <h2 className="results-display-head"><EmojiEventsOutlinedIcon style={{fontSize: "40px"}}/></h2>
+          <h2 className="results-display-head">
+            <EmojiEventsOutlinedIcon style={{ fontSize: "40px" }} />
+            Score: Display Best Here
+          </h2>
+          <Card style={{ width: "50%", margin: "0 auto" }}>
+            <CardContent>
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Round Number</TableCell>
+                      <TableCell>Round Score</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>#</TableCell>
+                      <TableCell>Score</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </CardContent>
+          </Card>
         </CardContent>
       </Card>
     </div>
