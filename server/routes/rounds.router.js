8588edaf567bdae01ed21866bc7a29879ed95151
T Mark <tmk1.tran@gmail.com>
2023-10-23 20:50:49 -0500

    updated VS code, initial commit on reload of platform

--- server/routes/rounds.router.js
+++ server/routes/rounds.router.js
@@ -97,6 +97,53 @@ router.post("/", rejectUnauthenticated, (req, res) => {
 //     });
 // });
 
+// router.post("/", (req, res) => {
+//   console.log(req.body);
+//   // RETURNING "id" will give us back the id of the created movie
+//   const insertMovieQuery = `
+//   INSERT INTO "movies" ("title", "poster", "description")
+//   VALUES ($1, $2, $3)
+//   RETURNING "id";`;
+
+//   // FIRST QUERY MAKES MOVIE
+//   pool
+//     .query(insertMovieQuery, [
+//       req.body.title,
+//       req.body.poster,
+//       req.body.description,
+//     ])
+//     .then((result) => {
+//       console.log("New Movie Id:", result.rows[0].id); //ID IS HERE!
+
+//       const createdMovieId = result.rows[0].id;
+
+//       // Now handle the genre reference
+//       const insertMovieGenreQuery = `
+//       INSERT INTO "movies_genres" ("movie_id", "genre_id")
+//       VALUES  ($1, $2);
+//       `;
+//       // SECOND QUERY ADDS GENRE FOR THAT NEW MOVIE
+//       pool
+//         .query(insertMovieGenreQuery, [createdMovieId, req.body.genre_id])
+//         .then((result) => {
+//           //Now that both are done, send back success!
+//           res.sendStatus(201);
+//         })
+//         .catch((err) => {
+//           // catch for second query
+//           console.log(err);
+//           res.sendStatus(500);
+//         });
+
+//       // Catch for first query
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.sendStatus(500);
+//     });
+// });
+
+
 router.delete("/:id", rejectUnauthenticated, (req, res) => {
   // endpoint functionality
   console.log("REQ: ", req.params.id);
