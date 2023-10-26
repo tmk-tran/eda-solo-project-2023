a9098e116ca36991dd2425dea13140dcf5e9b0d0
T Mark <tmk1.tran@gmail.com>
2023-10-25 19:19:51 -0500

    styling graphs in profile component, checking out for the day, returning when I get home

--- src/components/LineDot/LineDot.jsx
+++ src/components/LineDot/LineDot.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import { LineChart } from '@mui/x-charts/LineChart';
+import React from "react";
+import { LineChart } from "@mui/x-charts/LineChart";
+import { ChartsXAxis } from "@mui/x-charts/ChartsXAxis";
+import { ChartsYAxis } from "@mui/x-charts/ChartsYAxis";
 
 export default function LineDot() {
   return (
     <>
-    <LineChart
-      xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-      series={[
-        {
-          data: [2, 5.5, 2, 8.5, 1.5, 5],
-        },
-      ]}
-      width={500}
-      height={300}
-    />
+      <LineChart
+        xAxis={[{ label: "Rounds", data: [1, 2, 3, 5, 8, 10] }]}
+        series={[
+          {
+            label: "Score",
+            data: [2, 5.5, 2, 10.5, 1.5, 50],
+          },
+        ]}
+        width={500}
+        height={300}
+      />
     </>
   );
 }
