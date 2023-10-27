d0bb23fae821bbd7642bdb4b2280423dfa74e6de
T Mark <tmk1.tran@gmail.com>
2023-10-27 05:19:53 -0500

    more styling added throughoout app. changed font family in all places with text. going to gym, then back at it after

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, CardContent, Box, Paper, Typography } from "@mui/material";
+import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -19,16 +20,23 @@ function AboutPage() {
     >
       <Card elevation={6} style={{ height: "100%" }}>
         <CardContent>
-          <Typography variant="h4" color="primary" gutterBottom>
+          <Typography
+            variant="h4"
+            color="primary"
+            gutterBottom
+            style={{ fontFamily: "avenir" }}
+          >
             About ScoreMark
           </Typography>
-          <Typography variant="body1">
+          <Typography variant="body1" style={{ fontFamily: "avenir" }}>
             Welcome to ScoreMark! This app is designed to help target shooters
             track their shooting performance and improve their skills on the
             range.
           </Typography>
           <br />
-          <Typography variant="body1">Features:</Typography>
+          <Typography variant="body1" style={{ fontFamily: "avenir" }}>
+            Features:
+          </Typography>
           <ul>
             <li>Record and store your shooting scores</li>
             <li>Track your progress over time</li>
@@ -37,10 +45,32 @@ function AboutPage() {
             </li>
             <li>Record notes/goals to further measure your improvement</li>
           </ul>
-          <Typography variant="body1">
+          <Typography variant="body1" style={{ fontFamily: "avenir" }}>
             Whether you're a novice or an experienced shooter, ScoreMark can
             assist you in honing your skills and achieving your shooting goals.
           </Typography>
+          <br />
+          <Card elevation={7} style={{ backgroundColor: "yellow" }}>
+            <CardContent>
+              <Typography variant="h6">
+                <HealthAndSafetyIcon />
+                Safety:
+              </Typography>
+              <Typography variant="body1">Firearms:</Typography>
+              <Typography variant="body2" style={{ fontFamily: "avenir" }}>
+                Always treat every firearm as if it's loaded, keep it pointed in
+                a safe direction, and never touch the trigger until you're ready
+                to shoot
+              </Typography>
+              <br />
+              <Typography variant="body1">Archery:</Typography>
+              <Typography variant="body2" style={{ fontFamily: "avenir" }}>
+                Prioritize safety on the archery range: maintain a safe draw,
+                never point an arrow at anyone, and be aware of your
+                surroundings before releasing
+              </Typography>
+            </CardContent>
+          </Card>
         </CardContent>
       </Card>
     </Box>
--- src/components/HorizontalBars/HorizontalBars.jsx
+++ src/components/HorizontalBars/HorizontalBars.jsx
@@ -13,7 +13,7 @@ export default function HorizontalBars() {
 
   return (
     <BarChart
-      xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
+      xAxis={[{ scaleType: 'band', data: ['Game A', 'Game B', 'Game C'] }]}
       series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
       width={500}
       height={300}
