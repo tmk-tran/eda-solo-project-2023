92ecee00dc0ef279ec9da5544ac931c1f12cc43c
T Mark <tmk1.tran@gmail.com>
2023-10-28 19:39:10 -0500

    added new component, NewUser for login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import NewUser from "./NewUser";
 
 import {
   TextField,
@@ -34,81 +35,56 @@ function LoginForm() {
 
   return (
     <>
-      <form className="formPanel" onSubmit={login}>
-        <h2>Login</h2>
-        {errors.loginMessage && (
-          <h3 className="alert" role="alert">
-            {errors.loginMessage}
-          </h3>
-        )}
-        <div>
-          <label htmlFor="username">Username:</label>
-          <TextField
-            variant="outlined"
-            type="text"
-            name="username"
-            required
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">
-            Password:
-            <input
-              type="password"
-              name="password"
-              required
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </label>
-        </div>
-        <div>
-          <input className="btn" type="submit" name="submit" value="Log In" />
-        </div>
-      </form>
-      <Card id="login-card">
-        <CardContent>
-          <Typography variant="h5">Login</Typography>
-          <form onSubmit={login}>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifiyContent: "center",
-              }}
-            >
-              <TextField
-                variant="outlined"
-                type="username"
-                name="username"
-                label="Username"
-                required
-                value={username}
-                onChange={(event) => setUsername(event.target.value)}
-              />
-              <TextField
-                variant="outlined"
-                type="password"
-                name="password"
-                label="Password"
-                required
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-              />
-            </div>
-            <div>
-              <input
-                className="btn"
-                type="submit"
-                name="submit"
-                value="Log In"
-              />
-            </div>
-          </form>
-        </CardContent>
-      </Card>
+      <div style={{ display: "flex" }}>
+        <Card id="login-card" elevation={10} style={{ opacity: 0.7 }}>
+          <CardContent>
+            <Typography variant="h5">Login</Typography>
+            {errors.loginMessage && (
+              <h3 className="alert" role="alert">
+                {errors.loginMessage}
+              </h3>
+            )}
+            <form onSubmit={login}>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifiyContent: "center",
+                }}
+              >
+                <TextField
+                  variant="outlined"
+                  type="username"
+                  name="username"
+                  label="Username"
+                  required
+                  value={username}
+                  onChange={(event) => setUsername(event.target.value)}
+                />
+                <TextField
+                  variant="outlined"
+                  type="password"
+                  name="password"
+                  label="Password"
+                  required
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+              </div>
+              <div>
+                <Button
+                  variant="contained"
+                  type="submit"
+                  style={{ marginTop: "10px" }}
+                >
+                  Log In
+                </Button>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+        <NewUser />
+      </div>
     </>
   );
 }
--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import LoginForm from '../LoginForm/LoginForm';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import LoginForm from "../LoginForm/LoginForm";
+import { useHistory } from "react-router-dom";
+
+import { Typography, Card, CardContent, Button, Paper } from "@mui/material";
 
 function LoginPage() {
   const history = useHistory();
 
   return (
-    <div>
-      <LoginForm />
+    <>
+      <Paper id="login-paper">
+        <Paper style={{ opacity: 0.7 }}>
+        <Typography variant="h4">Welcome To ScoreMark</Typography>
+        </Paper>
+        <div>
+          <LoginForm />
 
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/registration');
-          }}
-        >
-          Register
-        </button>
-      </center>
-    </div>
+          <center>
+            <button
+              type="button"
+              className="btn btn_asLink"
+              onClick={() => {
+                history.push("/registration");
+              }}
+            >
+              Register
+            </button>
+          </center>
+        </div>
+      </Paper>
+    </>
   );
 }
 
