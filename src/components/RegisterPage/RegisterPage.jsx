f8ae7f8922ea765f66f2cd5f686c346aebf4458b
T Mark <tmk1.tran@gmail.com>
2023-10-29 23:02:39 -0500

    added registration form styling

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import { useHistory } from 'react-router-dom';
-import RegisterForm from '../RegisterForm/RegisterForm';
+import { useHistory } from "react-router-dom";
+import RegisterForm from "../RegisterForm/RegisterForm";
+import { Button, Typography, Card, CardContent } from "@mui/material";
 
 function RegisterPage() {
   const history = useHistory();
@@ -11,15 +12,23 @@ function RegisterPage() {
       <RegisterForm />
 
       <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/login');
-          }}
-        >
-          Login
-        </button>
+        <br />
+        <Card style={{ width: "75%" }}>
+          <CardContent>
+          <Typography variant="h6">
+            Have an account?{" "}
+            <Button
+              variant="outlined"
+              onClick={() => {
+                history.push("/login");
+              }}
+            >
+              Login
+            </Button>{" "}
+            Here
+          </Typography>
+          </CardContent>
+        </Card>
       </center>
     </div>
   );
